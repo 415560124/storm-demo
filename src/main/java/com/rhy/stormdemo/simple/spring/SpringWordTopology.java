4902776988737aba80c6a7f6839433629fd2eafe
Rhy <415560124@qq.com>
2021-08-13 18:01:56 +0800

    1.分组策略代码 2.WordCount代码示例

--- src/main/java/com/rhy/stormdemo/simple/spring/SpringWordTopology.java
+++ src/main/java/com/rhy/stormdemo/simple/spring/SpringWordTopology.java
@@ -1,7 +1,24 @@
 package com.rhy.stormdemo.simple.spring;
 
+import com.rhy.stormdemo.simple.word.WordCountBolt;
+import com.rhy.stormdemo.simple.word.WordSplitBolt;
+import com.rhy.stormdemo.simple.word.WordSpout;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.topology.TopologyBuilder;
+
 public class SpringWordTopology {
     public static void main(String[] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("wordSpout",new WordSpout());
+        builder.setBolt("wordSplitBolt",new WordSplitBolt()).shuffleGrouping("wordSpout");
+        builder.setBolt("wordCountBolt",new WordCountBolt()).shuffleGrouping("wordSplitBolt");
 
+        Config config = new Config();
+//        config.setNumWorkers(3);
+        StormTopology topology = builder.createTopology();
+        LocalCluster localCluster = new LocalCluster();
+        localCluster.submitTopology("wordTopology",config,topology);
     }
 }
